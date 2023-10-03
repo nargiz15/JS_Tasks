8f3e7d129d13154c6a0c5e1aa3afa1a1784fd159
Nargiz Azizli <143933432+nargiz15@users.noreply.github.com>
2023-10-03 03:20:37 -0700

    finish task_7 (if)

---
+++ task_7/app.js
@@ -0,0 +1,9 @@
+// Istifadeciden eded alin. Eger eded 2 reqemlidirse, console-a "eded ikireqemlidir" cixsin.
+
+let value =Number(prompt("bir eded qeyd edin"))
+if(value>9 && value<100){
+    alert("eded iki reqemlidir")
+}
+else{
+    alert( "error")
+}

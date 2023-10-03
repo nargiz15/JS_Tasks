64874fe59712f01244cdebfccd3482b6eccd75fc
Nargiz Azizli <143933432+nargiz15@users.noreply.github.com>
2023-10-03 02:21:43 -0700

    Finish task4

---
+++ task_4/app.js
@@ -0,0 +1,10 @@
+//ededin  kvadrat kokunu tapmaq ucun proqram yazin
+
+let value = Number(prompt("eded daxil edin"));
+value = value**(1/2);
+alert(`daxil etdiyiniz ededin koku ${value} -dur `)
+
+
+
+
+

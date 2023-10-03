4240b1a2993dafd1b117798d12999afe56605940
Nargiz Azizli <143933432+nargiz15@users.noreply.github.com>
2023-10-03 03:43:44 -0700

    finish task_8 (if)

---
+++ task_8/app.js
@@ -0,0 +1,6 @@
+//  Istifadeciden eded alin. Eger eded hem 2ye ve hem de 5e bolunerse, console-a "bu istediyim ededdir" cixsin. 
+
+let eded=Number(prompt("bir eded yazin"))
+if(eded%2 ===0 && eded%5 ===0){
+    alert("bu istediyim ededdir")
+}

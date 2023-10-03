a3961e4eb6f0fad0b3ebe9509d228714086e46b7
Nargiz Azizli <143933432+nargiz15@users.noreply.github.com>
2023-10-03 02:31:22 -0700

    Finish task_5

---
+++ task_5/app.js
@@ -0,0 +1,6 @@
+
+//selsi deyerini faranheite cevirin
+let value = Number(prompt("dereceni selsi ile daxil edin"));
+let result= value*1.8+32;
+alert(`${value} derece selsi ${result} faranheite beraberdir`)
+

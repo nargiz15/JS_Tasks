4d78add47ad881d5a30df71a741f8d8162d7fd3d
Nargiz Azizli <143933432+nargiz15@users.noreply.github.com>
2023-10-03 04:06:28 -0700

    finish task_9 (if)

---
+++ task_9/app.js
@@ -0,0 +1,11 @@
+// 3 eded daxil edin. Onlarin cemi eger 100-den cox olarsa, console-a "cemleri kecdi 100u" cixsin.
+let eded1 =Number(prompt("10"))
+let eded2 =Number(prompt("20"))
+let eded3 =Number(prompt("30"))
+sum=eded1+eded2+eded3
+if(sum>100){
+    alert("cemleri kecdi 100u")
+}else{
+    alert("cemleri kecmedi 100u")
+
+}

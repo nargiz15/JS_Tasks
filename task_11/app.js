7a7e14b7db8ddbf4995ce26b7be7288f2f54650b
Nargiz Azizli <143933432+nargiz15@users.noreply.github.com>
2023-10-05 03:20:36 -0700

    add task _ 11 no change code

--- task_11/app.js
+++ task_11/app.js
@@ -1,7 +1,8 @@
 // Rəqəmin tək və ya cüt olduğunu yoxlamaq üçün JavaScript proqramı yazın.(if...else istifadə edin)
 
 let eded=+prompt("eded daxil edin")
-let result= eded%2
+let result= eded % 2
+
 if(result===0)(
     console.log("edediniz cutdur")
 )

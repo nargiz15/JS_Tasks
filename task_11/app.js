cdc95e0b383ba3fa0ca660fe5d2a88ee2c805ae6
Nargiz Azizli <143933432+nargiz15@users.noreply.github.com>
2023-10-05 02:42:35 -0700

    Finish task_11 (if)

---
+++ task_11/app.js
@@ -0,0 +1,12 @@
+// Rəqəmin tək və ya cüt olduğunu yoxlamaq üçün JavaScript proqramı yazın.(if...else istifadə edin)
+
+let eded=+prompt("eded daxil edin")
+let result= eded%2
+if(result===0)(
+    console.log("edediniz cutdur")
+)
+
+else{
+    console.log("edediniz tekdir")
+}
+    

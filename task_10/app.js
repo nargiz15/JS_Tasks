7fd5d96825bd34193997502c7b51058cedd21eec
Nargiz Azizli <143933432+nargiz15@users.noreply.github.com>
2023-10-05 02:34:09 -0700

    finish task_10 (if)

---
+++ task_10/app.js
@@ -0,0 +1,19 @@
+
+
+// // Istifadəçinin daxil etdiyi nömrənin müsbət, mənfi və ya sıfır olduğunu təyin edin və
+//  göstərin(console.log)(if...else istifadə edin) */
+
+let eded = +prompt("eded daxil edin")
+
+if(eded===0){
+    console.log(eded, 'menfi');
+
+}
+
+else if(eded>0){
+    console.log(eded, 'musbet');
+}
+else if(eded<0){
+     console.log(eded, 'menfi');
+
+}

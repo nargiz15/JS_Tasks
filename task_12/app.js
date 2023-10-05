c418186bd4741298d4fbbcefe3fc78357208bc78
Nargiz Azizli <143933432+nargiz15@users.noreply.github.com>
2023-10-05 03:24:55 -0700

    add

--- task_12/app.js
+++ task_12/app.js
@@ -6,6 +6,7 @@ let eded3 = +prompt('reqem daxil et 3 cu')
   // 5 boyuk oldu true   //  true 
 if(eded1 > eded2  && eded1 > eded3){
     console.log("eded 1 boyukdur");
+    
 }
 
 else if(eded2 > eded3 && eded2 > eded1){

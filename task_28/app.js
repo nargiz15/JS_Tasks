deda5359888cc5c85dbfd64be7ffcca2bd0cb23c
Nargiz Azizli <143933432+nargiz15@users.noreply.github.com>
2023-11-05 03:53:48 -0800

    Lowercase method

---
+++ task_28/app.js
@@ -0,0 +1,13 @@
+// snakeToKebab(str) -=> SNAKE_CASE to kebab-case -=> BU_TEST_UCUNDUR -> bu-test-ucundur
+
+// let value = ("snakeTokebab")
+// let result =("")
+// result = value.concat("snake", "_", "Case" + " kebab-case")
+// console.log(result);
+
+
+
+const value = ("BU_TEST_UCUNDUR")
+result = value.toLowerCase( )
+console.log(result);
+
